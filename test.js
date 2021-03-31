39d3a701bd4f28150b68f198ebcab6d45c40c88f
jfadgyas <djdeville72@yahoo.com>
2021-03-31 06:24:50 +0200

    ios9 test 14

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ const para = document.getElementById('text')
 
 para.innerHTML = window.navigator.userAgent
 
-bttn.addEventListener('touchstart', function ts() {para.innerHTML='clicked'})
+bttn.addEventListener('touchstart', () => para.innerHTML='clicked')
