a973aefb7c796a38bcb5d95f19b88577d911f0e3
jfadgyas <djdeville72@yahoo.com>
2021-03-31 06:43:38 +0200

    ios9 test 18

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const bttn = document.querySelector('#btn')
 const para = document.getElementById('text')
+const div = document.querySelector('#divclick')
 
 para.innerHTML = window.navigator.userAgent
 
 bttn.addEventListener('click', () => para.innerHTML='clicked')
+div.addEventListener('click', () => para.innerHTML='div is clicked')
 console.log('test')
--- test_ios9.js
+++ test_ios9.js
@@ -1,7 +1,9 @@
 const bttn = document.querySelector('#btn')
 const para = document.getElementById('text')
+const div = document.querySelector('#divclick')
 
 para.innerHTML = window.navigator.userAgent
 
 bttn.addEventListener('touchstart', function ios9() {para.innerHTML='clicked'})
+div.addEventListener('touchstart', function iosdiv() {para.innerHTML='div is clicked'})
 console.log('test_ios')
