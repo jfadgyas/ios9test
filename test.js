fbb72cbba0007c55fe27765eca4879623c39912e
jfadgyas <djdeville72@yahoo.com>
2021-04-04 13:06:17 +0200

    width test

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ const bttn = document.querySelector('#btn')
 const para = document.getElementById('text')
 const div = document.querySelector('#divclick')
 
-para.innerHTML = window.navigator.userAgent
+para.innerHTML = window.innerWidth
+para.append(screen.width)
 
 bttn.addEventListener('click', () => para.innerHTML='clicked')
 div.addEventListener('click', () => para.innerHTML='div is clicked')
