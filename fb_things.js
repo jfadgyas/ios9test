e9cd82b438aced6b6da7608a08a9edfe1ca8c37a
jfadgyas <djdeville72@yahoo.com>
2021-04-23 05:55:57 +0200

    select test

--- fb_things.js
+++ fb_things.js
@@ -6,18 +6,36 @@ window.fbAsyncInit = function() {
       version          : 'v10.0'
     });
     console.log('FB-loaded')
-    
-    // FB.getLoginStatus(function(response) {
-    //     console.log(response);
-    // });
 
+    const page_id = 320746671665588 //ugly kitchen//sajat4073071839410612
+    const access_token = 'EAA7ZAUZBDdHgoBAOgTyHFAW5Up4b6THb5Jd6eyWSumprTAv1uI3qbSkHhZCEObZBU4BQ21JiRppTfml7Xkjut0LnQYBaDNLucXVZCj3ZAG4EumqDoCDCnB5WSyeZBsK24Doz6OxpyKzmpI0aNmIEtI1y4BW3gGYGasoTILdDcljYfoaXZBO8112DzdKjtwVRPsZBlkeCRFjbGtgZDZD'
+    
+    const fb_name = document.querySelector('#fb_name')
+    const fb_profilePic = document.querySelector('#profile')
+    const albums = document.querySelector('#albums')
+    
     FB.api(
-        "/https://www.facebook.com/janos.fadgyas.10",
-        function (response) {
+        `/${page_id}?access_token=${access_token}`,
+        'GET',
+        {fields: "name, picture, albums{name, cover_photo{picture}}"},
+        function(response) {
+            // Insert your code here
             console.log(response)
-          if (response && !response.error) {
-          }
+            fb_name.innerHTML = response.name
+            fb_profilePic.src = `${response.picture.data.url}`
+            response.albums.data.map(item => {
+                const newAlbum = document.createElement('li')
+                const albumName = document.createElement('p')
+                const albumPic = document.createElement('img')
+
+                albumName.innerHTML = item.name
+                albumPic.src= item.cover_photo.picture
+
+                newAlbum.appendChild(albumName)
+                newAlbum.appendChild(albumPic)
+                albums.appendChild(newAlbum)
+            })
         }
-    );
+      );
 
   };
