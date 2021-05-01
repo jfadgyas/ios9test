3d71f56a03cce1480fbabbcabc49bf1254166d7d
jfadgyas <djdeville72@yahoo.com>
2021-05-01 12:37:55 +0200

    media queries

--- test_ios9.js
+++ test_ios9.js
@@ -3,6 +3,8 @@ const para = document.getElementById('text')
 const div = document.querySelector('#divclick')
 
 para.innerHTML = window.navigator.userAgent
+para.append = window.innerWidth
+para.append(screen.width)
 
 bttn.addEventListener('touchstart', function ios9() {para.innerHTML='clicked'})
 div.addEventListener('touchstart', function iosdiv() {para.innerHTML='div is clicked'})
