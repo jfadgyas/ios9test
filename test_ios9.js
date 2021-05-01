54e706b09c59c0cdb1099d0fbea944b337e5cae8
jfadgyas <djdeville72@yahoo.com>
2021-05-01 12:41:37 +0200

    media queries

--- test_ios9.js
+++ test_ios9.js
@@ -2,9 +2,9 @@ const bttn = document.querySelector('#btn')
 const para = document.getElementById('text')
 const div = document.querySelector('#divclick')
 
-// para.innerHTML = window.navigator.userAgent
+para.innerHTML = window.navigator.userAgent
 // para.innerHTML = window.innerWidth
-para.innerHTML(screen.width)
+// para.innerHTML(screen.width)
 
 bttn.addEventListener('touchstart', function ios9() {para.innerHTML='clicked'})
 div.addEventListener('touchstart', function iosdiv() {para.innerHTML='div is clicked'})
