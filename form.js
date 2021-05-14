011b0af2944885a99794727a15ead4b073cdb93f
jfadgyas <djdeville72@yahoo.com>
2021-05-14 20:45:18 +0200

    form validation

--- form.js
+++ form.js
@@ -2,18 +2,18 @@ const form = document.querySelector('#frm')
 const button = document.querySelector('#send')
 
 const validate = e => {
+    e.preventDefault()
     let z=Array.from(form.querySelectorAll('input'))
     z.map(item => {
         console.log(item.validity)
         if (item.validity.valueMissing){
             item.setCustomValidity('hello')
             item.reportValidity()
-            e.preventDefault()
         }
-        else {
-            item.setCustomValidity('')
-            item.reportValidity()
-        }
+        // else {
+        //     item.setCustomValidity('')
+        //     item.reportValidity()
+        // }
     })
 }
 
