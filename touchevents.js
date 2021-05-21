86a865c320edf7b1d55579f1564b3ef8ad718db4
jfadgyas <djdeville72@yahoo.com>
2021-05-21 17:33:49 +0200

    sliding gallery

--- touchevents.js
+++ touchevents.js
@@ -3,16 +3,19 @@ box = document.querySelector('#dragbox')
 const ts = e => {
     e.preventDefault()
     console.log('start')
+    box.innerHTML='start'
 }
 
 const tm = e => {
     e.preventDefault()
     console.log('move')
+    box.innerHTML='move'
 }
 
 const te = e => {
     e.preventDefault()
     console.log('end')
+    box.innerHTML='end'
 }
 
 box.addEventListener('touchstart', (e) => ts(e))
