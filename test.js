1bab0dfa3b0f49269055e30b479061407406caf1
jfadgyas <djdeville72@yahoo.com>
2022-09-11 16:48:00 +0200

    button test

--- test.js
+++ test.js
@@ -8,3 +8,21 @@ para.append(screen.width)
 bttn.addEventListener('click', () => para.innerHTML='clicked')
 div.addEventListener('click', () => para.innerHTML='div is clicked')
 console.log('test')
+
+// text scrolling
+const cells = Array.from(document.querySelectorAll('.cella'))
+
+cells.map(item => {
+    // console.log(item)
+    if (item.clientWidth < item.scrollWidth) item.classList.add('animate')
+})
+
+// hidden buttons under the row
+const list = document.querySelector('#productlist')
+list.addEventListener('click', e => {
+    const item = e.target.closest('div')
+    item.classList.toggle('moved')
+    const btn = e.target.closest('button')
+    console.log(btn)
+
+})
